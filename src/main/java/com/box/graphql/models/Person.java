9c1b68c71b646cddbc63ca09099437480443ba68
jackwu <p500.wu@gmail.com>
2019-10-18 15:39:08 +0800

    init

---
+++ src/main/java/com/box/graphql/models/Person.java
@@ -0,0 +1,26 @@
+package com.box.graphql.models;
+
+import javax.persistence.*;
+
+
+@Entity
+@TAble(name = "person")
+class Person {
+    
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uuid;
+
+    @Column(length = 50)
+    private String name;
+
+    private int age;
+    
+    public int getUuid() { return uuid; }
+    public void setUuid(int uuid) { this.uuid = uuid; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+}
